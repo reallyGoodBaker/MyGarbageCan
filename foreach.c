498c9c1d269884c3dbafe173cdb2aed2024de68e
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2021-12-06 12:33:00 +0800

    del some files

--- foreach.c
+++
@@ -1,7 +0,0 @@
-typedef void (*ForeachCallback)(int data, size_t index, int arr[]);
-
-void forEach(int arr[], size_t len, ForeachCallback callback) {
-	for(int i = 0; i < len; i++) {
-		callback(arr[i], i, arr);
-	}
-}
